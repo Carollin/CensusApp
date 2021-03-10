40bc095dcd9111745dbc54e6ee6fbae7dcde9991
Carollin <78158741+Carollin@users.noreply.github.com>
2021-03-09 22:59:31 -0600

    Update app.R

--- App-1/app.R
+++ App-1/app.R
@@ -1,54 +1,59 @@
-# Define UI for app that draws a histogram ----
+library(maps)
+library(mapproj)
+source("/Users/apple/RstudioProjects/CensusApp/helpers.R")
+counties <- readRDS("/Users/apple/RstudioProjects/CensusApp/data/counties.rds")
+percent_map(counties$white, "darkgreen", "% White")
+
 ui <- fluidPage(
+  titlePanel("censusVis"),
   
-  # App title ----
-  titlePanel("Hello Shiny!"),
-  
-  # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
-    # Sidebar panel for inputs ----
     sidebarPanel(
+      helpText("Create demographic maps with 
+        information from the 2010 US Census."),
       
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
       
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
     ),
     
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-      
-    )
+    mainPanel(plotOutput("map"))
   )
 )
-# Define server logic required to draw a histogram ----
+
+# Server logic ----
 server <- function(input, output) {
-  
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
+  output$map <- renderPlot({
+    data <- switch(input$var, 
+                   "Percent White" = counties$white,
+                   "Percent Black" = counties$black,
+                   "Percent Hispanic" = counties$hispanic,
+                   "Percent Asian" = counties$asian)
     
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    color <- switch(input$var, 
+                    "Percent White" = "darkgreen",
+                    "Percent Black" = "black",
+                    "Percent Hispanic" = "darkorange",
+                    "Percent Asian" = "darkviolet")
     
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
+    legend <- switch(input$var, 
+                     "Percent White" = "% White",
+                     "Percent Black" = "% Black",
+                     "Percent Hispanic" = "% Hispanic",
+                     "Percent Asian" = "% Asian")
     
+    percent_map(data, color, legend, input$range[1], input$range[2])
   })
-  
 }
+
+# Run app ----
+shinyApp(ui, server)
+runGitHub("CensusApp","Carollin")
+
 
